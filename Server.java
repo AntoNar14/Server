3b1759f13cb52eb94e7c9fc737d461ce4b2f96a2
Giacomo Vigilante <126689849+Gipivigi@users.noreply.github.com>
2024-05-21 10:40:38 +0200

    Implementata funzione per la registrazione degli utenti

--- Server.java
+++ Server.java
@@ -1,17 +1,17 @@
 import java.net.*;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Server
 {
     // Variabili globali del server necessarie ad elaborare la risposta
-    static int cont=0;
-    static int pagamenti=0, prelievi=0, raccomandate=0, serviziTelefonici=0;
+    static ArrayList<Utente> listaUtentiCollegati=new ArrayList<Utente>();
     public static void main(String args[])
     {
         ServerSocket ss;
         try{
             ss = new ServerSocket(55555);
-            while(true)
-            {
                 try{
                         Socket client = ss.accept();
                         System.out.println("Accettata connessione da: " + client.getRemoteSocketAddress().toString());
@@ -19,7 +19,15 @@ public class Server
                         BufferedWriter out = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
                         // Lettura richiesta dal client
                         String str=in.readLine();
-                        String risposta = elaborazione(str);
+                        String richiesta="";
+                        String informazioni="";
+                        if(str!=null && str.length()>1)
+                        {
+                            richiesta=str.substring(0,1);
+                            informazioni=str.substring(1);
+                        }
+
+                        String risposta = elaborazione(richiesta,informazioni);
                         //trasmissione risposta del server
                         out.write(risposta);
                         out.flush();
@@ -33,25 +41,52 @@ public class Server
                             e.getMessage());
                 }
             }
-        }
         catch(Exception e) {
             System.out.println("APERTURA ServerSocket FALLITA!\nErrore: " +e.getMessage());
         }
     }
 
-    static private String elaborazione(String richiesta)throws Exception
+    private static String elaborazione(String richiesta, String informazioni)throws Exception
     {
         switch(richiesta)
         {
-            case "PA": pagamenti++;
-                return "PA"+pagamenti;
-            case "PR": prelievi++;
-                return "PR"+prelievi;
-            case "RA": raccomandate++;
-                return "PR"+raccomandate;
-            case "ST": serviziTelefonici++;
-                return "PR"+serviziTelefonici;
+            case "L": return registraUtente(informazioni);
             default: throw new Exception("Operazione non valida");
         }
+    }
+
+    private static String registraUtente(String nomeUtente) throws Exception
+    {
+        if(nomeUtente.contains("|"))
+        {
+            return "E";
+        }
+        for(int i=0; i<listaUtentiCollegati.size(); i++)
+        {
+            if(nomeUtente.equals(listaUtentiCollegati.get(i).nomeUtente))
+                return "A";
+        }
+        String id=generaID();
+        listaUtentiCollegati.add(new Utente(nomeUtente,id));
+        return id;
+    }
+
+    private static String generaID()
+    {
+        Random random = new Random();
+        String caratteri="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        String id="";
+        for(int i=0; i<10; i++)
+        {
+            id+=caratteri.charAt(random.nextInt(caratteri.length()));
+        }
+
+        for(int i=0; i<listaUtentiCollegati.size(); i++)
+        {
+            if(id.equals(listaUtentiCollegati.get(i).IDUtente))
+                id=generaID();
+        }
+
+        return id;
     }
 }
